3a34a31cb66e9f993395729921585f90dc8cbf6e
Vitaliy Filyuchkov <philyuchkoff@gmail.com>
2020-12-21 19:02:19 +0300

    Update and rename install-openssl_1.1.1h.sh to install-openssl_1.1.1i.sh

--- install-openssl_1.1.1i.sh
+++ install-openssl_1.1.1i.sh
@@ -17,11 +17,11 @@ yum -y install \
 yum -y remove openssl
 
 # Get openssl tarball
-curl -O --silent https://www.openssl.org/source/openssl-1.1.1h.tar.gz
+curl -O --silent https://www.openssl.org/source/openssl-1.1.1i.tar.gz
 
 # SPEC file
 cat << 'EOF' > ~/openssl/openssl.spec
-Summary: OpenSSL 1.1.1h for Centos
+Summary: OpenSSL 1.1.1i for Centos
 Name: openssl
 Version: %{?version}%{!?version:1.1.1h}
 Release: 1%{?dist}
@@ -37,8 +37,8 @@ BuildRoot: %{_tmppath}/%{name}-%{version}-%{release}-root
 %global openssldir /usr/openssl
 
 %description
-https://github.com/philyuchkoff/openssl-1.1.1h-RPM-Builder
-OpenSSL RPM for version 1.1.1h on Centos
+https://github.com/philyuchkoff/openssl-1.1.1i-RPM-Builder
+OpenSSL RPM for version 1.1.1i on Centos
 
 %package devel
 Summary: Development files for programs which will use the openssl library
@@ -46,7 +46,7 @@ Group: Development/Libraries
 Requires: %{name} = %{version}-%{release}
 
 %description devel
-OpenSSL RPM for version 1.1.1h on Centos (development package)
+OpenSSL RPM for version 1.1.1i on Centos (development package)
 
 %prep
 %setup -q
@@ -88,13 +88,13 @@ EOF
 mkdir -p /root/rpmbuild/{BUILD,RPMS,SOURCES,SPECS,SRPMS}
 cp ~/openssl/openssl.spec /root/rpmbuild/SPECS/openssl.spec
 
-mv openssl-1.1.1h.tar.gz /root/rpmbuild/SOURCES
+mv openssl-1.1.1i.tar.gz /root/rpmbuild/SOURCES
 cd /root/rpmbuild/SPECS && \
     rpmbuild \
-    -D "version 1.1.1h" \
+    -D "version 1.1.1i" \
     -ba openssl.spec
 
 
-# For install:  rpm -ivvh /root/rpmbuild/RPMS/x86_64/openssl-1.1.1h-1.el7.x86_64.rpm --nodeps
+# For install:  rpm -ivvh /root/rpmbuild/RPMS/x86_64/openssl-1.1.1i-1.el7.x86_64.rpm --nodeps
 # Verify install:  rpm -qa openssl
 #                  openssl version
