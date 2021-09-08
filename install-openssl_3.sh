0f9bdf5b02f050d7a6aa02652fd7d9c929b9be60
Vitaliy Filyuchkov <philyuchkoff@gmail.com>
2021-09-08 16:55:03 +0300

    OpenSSL 3.0.0
    OpenSSL 3.0.0

---
+++ install-openssl_3.sh
@@ -0,0 +1,96 @@
+#!/bin/bash
+
+# Падаем сразу, если возникли какие-то ошибки
+set -e
+# Выводим, то , что делаем
+set -v
+mkdir ~/openssl && cd ~/openssl
+yum -y install \
+    curl \
+    which \
+    make \
+    gcc \
+    perl \
+    perl-WWW-Curl \
+    rpm-build \
+    perl-IPC-Cmd
+
+yum -y remove openssl
+
+# Get openssl tarball
+curl -O --silent https://www.openssl.org/source/openssl-3.0.0.tar.gz
+
+# SPEC file
+cat << 'EOF' > ~/openssl/openssl3.spec
+Summary: OpenSSL 3.0.0 for Centos
+Name: openssl
+Version: %{?version}%{!?version:3.0.0}
+Release: 1%{?dist}
+Obsoletes: %{name} <= %{version}
+Provides: %{name} = %{version}
+URL: https://www.openssl.org/
+License: GPLv2+
+
+Source: https://www.openssl.org/source/%{name}-%{version}.tar.gz
+
+BuildRequires: make gcc perl perl-WWW-Curl
+BuildRoot: %{_tmppath}/%{name}-%{version}-%{release}-root
+%global openssldir /usr/openssl
+
+%description
+https://github.com/philyuchkoff/openssl-RPM-Builder
+OpenSSL RPM for version 3.0.0 on CentOS
+
+%package devel
+Summary: Development files for programs which will use the openssl library
+Group: Development/Libraries
+Requires: %{name} = %{version}-%{release}
+
+%description devel
+OpenSSL RPM for version 3.0.0 on CentOS (development package)
+
+%prep
+%setup -q
+
+%build
+./config --prefix=%{openssldir} --openssldir=%{openssldir}
+make
+
+%install
+[ "%{buildroot}" != "/" ] && %{__rm} -rf %{buildroot}
+%make_install
+
+mkdir -p %{buildroot}%{_bindir}
+mkdir -p %{buildroot}%{_libdir}
+ln -sf %{openssldir}/lib/libssl.so.1.1 %{buildroot}%{_libdir}
+ln -sf %{openssldir}/lib/libcrypto.so.1.1 %{buildroot}%{_libdir}
+ln -sf %{openssldir}/bin/openssl %{buildroot}%{_bindir}
+
+%clean
+[ "%{buildroot}" != "/" ] && %{__rm} -rf %{buildroot}
+
+%files
+%{openssldir}
+%defattr(-,root,root)
+/usr/bin/openssl
+/usr/lib64/libcrypto.so.1.1
+/usr/lib64/libssl.so.1.1
+
+%files devel
+%{openssldir}/include/*
+%defattr(-,root,root)
+
+%post -p /sbin/ldconfig
+
+%postun -p /sbin/ldconfig
+EOF
+
+
+mkdir -p /root/rpmbuild/{BUILD,RPMS,SOURCES,SPECS,SRPMS}
+cp ~/openssl/openssl3.spec /root/rpmbuild/SPECS/openssl.spec
+
+mv openssl-3.0.0.tar.gz /root/rpmbuild/SOURCES
+cd /root/rpmbuild/SPECS && \
+    rpmbuild \
+    -D "version 3.0.0" \
+    -ba openssl.spec
